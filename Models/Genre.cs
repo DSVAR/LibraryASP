97529b372b78f9b10eac6346677e3f16c1283945
Nyrmehametov danil <danilka468@outlook.com>
2020-09-08 15:39:00 +0300

    1тест

---
+++ Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public enum Genre
+    {
+        Роман,
+        Дектетив,
+        Триллер,
+        Проза,
+        Стихи,
+        Рассказ,
+        Приключения,
+        Сага
+    }
+}
---
+++ Models/books.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+    public class books
+    { 
+        public int id { get; set; }
+        public string Author { get; set; }
+        public string description { get; set; }
+        public string PhotoPath { get; set; }
+        public Genre? Genres { get; set; }
+    }
+}
